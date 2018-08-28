279a84b4a3057d49242acd1ad82e820a3afde667
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 00:07:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ class Driver {
   this.id = id;
   this.name = name;
   }
+
+  trips() {
+
+  }
 }
 
 class Passenger {
-  constructor(name, driverId) {
+  constructor(id, name, driverId) {
     this.name = name;
     this.driverId = driver.id;
   }
+}
+
+class Trip {
+  constructor(id, driverId, passengerId)
 }
