f1ea3e46ea8ee130670a0b50978fd884a659f650
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 00:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ class Driver {
   constructor(name) {
   this.id = ++driverId;
   this.name = name;
-
-  store.drivers.push(this)
+  store.drivers.push(this);
+}
 
   trips() {
-
+  return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
   }
 
   passengers() {
-
-  }
+    return this.trips().map(trip => {
+         return trip.passenger();
+       });
 }
 }
 
